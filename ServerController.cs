217ee187261a85846f2e8bedaf56dc50d2c1d66e
BioApfelsaft <BioApfelsaft@localhost>
2017-11-13 15:47:13 +0100

    init

---
+++ ServerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Modules;
+using Unosquare.Labs.EmbedIO.Constants;
+using Unosquare.Net;
+using Newtonsoft.Json;
+
+//#TODO token
+
+public class ServerController : WebApiController {
+
+    ServerList _serverList;
+
+    public ServerController(ServerList serverList) {
+        _serverList = serverList;
+    }
+
+    /// <summary>
+    /// Must be called from server to register or update details.
+    /// </summary>
+    /// <param name="server">Set by EmbedIO Framework</param>
+    /// <param name="context">Set by EmbedIO Framework</param>
+    [WebApiHandler(HttpVerbs.Put, "/api/v1/server/put")]
+    public bool Put(WebServer server, HttpListenerContext context) {
+        try {
+           var details = JsonConvert.DeserializeObject<ServerDetails>(context.RequestBody());
+
+            if (details.address == null || details.address.Length == 0)
+                details.address = context.Request.RemoteEndPoint.Address.ToString();   //#TODO IPv6 ?
+
+            _serverList.Add(details);
+
+            return true;
+        } catch (JsonReaderException e) {
+            return HandleError(context, e.Message);
+        }
+    }
+
+    /// <summary>
+    /// Called from clients to query all registered servers.
+    /// </summary>
+    /// <param name="server">Set by EmbedIO Framework</param>
+    /// <param name="context">Set by EmbedIO Framework</param>
+    [WebApiHandler(HttpVerbs.Get, "/api/v1/server/query")]
+    public bool List(WebServer server, HttpListenerContext context) {
+        _serverList.RemoveOldServers();
+
+        var result = new {
+            count = _serverList.server.Count,
+            server = new List<ServerDetails>()
+        };
+
+        foreach (var entry in _serverList.server)
+            result.server.Add(entry.Value);
+
+        try {
+            return context.JsonResponse(JsonConvert.SerializeObject(result));
+        } catch (JsonReaderException e) {
+            return HandleError(context, e.Message);
+        }
+    }
+
+    protected bool HandleError(HttpListenerContext context, string message, int statusCode = 500) {
+        var errorResponse = new {
+            Message = message,
+            StatusCode = statusCode
+        };
+
+        context.Response.StatusCode = statusCode;
+        return context.JsonResponse(errorResponse);
+    }
+
+}
---
+++ ServerDetails.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+/// <summary>
+/// 
+/// </summary>
+public class ServerDetails {
+
+    public int id {
+        get { return GetHashCode(); }
+    }
+
+    [JsonIgnore]
+    DateTime _lastUpdated;
+    public DateTime lastUpdated {
+        get { return _lastUpdated; }
+    }
+
+    public string address;
+
+    [JsonRequired]
+    public short port;
+
+    [JsonRequired]
+    public string version;
+
+    [JsonRequired]
+    public short players;
+
+    [JsonRequired]
+    public short maxPlayers;
+
+    [JsonRequired]
+    public string title;
+
+    public ServerDetails() {
+        _lastUpdated = DateTime.Now;
+    }
+
+    public override bool Equals(object obj) {
+        ServerDetails other = (ServerDetails)obj;
+        if (other == null)
+            throw new InvalidCastException();
+
+        return other.address == address && other.port == port;
+    }
+
+    public override int GetHashCode() {
+        return (address + port.ToString()).GetHashCode();
+    }
+}
